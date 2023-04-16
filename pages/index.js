d6810592e8f358695fc891a240bde60ae7b74cbd
signtalkcode <125157024+signtalkcode@users.noreply.github.com>
2023-04-16 12:32:29 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -36,41 +36,22 @@ export default function Home() {
   const [camState, setCamState] = useState("on")
 
   const [sign, setSign] = useState(null)
-
-  let signList = []
+  
   let currentSign = 0
 
   let gamestate = "started"
 
-  // let net;
-
-  async function runHandpose() {
-    const net = await handpose.load()
-    _signList()
-
-    // window.requestAnimationFrame(loop);
-
-    setInterval(() => {
-      detect(net)
-    }, 150)
-  }
-
-  function _signList() {
-    signList = generateSigns()
-  }
-
-  function shuffle(a) {
-    for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1))
-      ;[a[i], a[j]] = [a[j], a[i]]
+  useEffect(() => {
+    async function runHandpose() {
+      const net = await handpose.load()
+      console.log("Handpose model loaded.")
+      setInterval(() => {
+        detect(net)
+      }, 150)
     }
-    return a
-  }
 
-  function generateSigns() {
-    const password = shuffle(Signpass)
-    return password
-  }
+    runHandpose()
+  }, [])
 
   async function detect(net) {
     // Check data is available
@@ -127,187 +108,87 @@ export default function Home() {
           Handsigns.zSign,
         ])
 
-        const estimatedGestures = await GE.estimate(hand[0].landmarks, 6.5)
-        // document.querySelector('.pose-data').innerHTML =JSON.stringify(estimatedGestures.poseData, null, 2);
+          const gesture = await GE.estimate(hand[0].landmarks, 4)
 
-        if (gamestate === "started") {
-          document.querySelector("#app-title").innerText =
-            "Make a 👍 gesture with your hand to start"
+    if (gesture.gestures !== undefined && gesture.gestures.length > 0) {
+      const confidence = gesture.gestures.map((prediction) => prediction.confidence)
+      const maxConfidence = confidence.indexOf(Math.max.apply(null, confidence))
+
+      setSign(gesture.gestures[maxConfidence].name)
+
+      if (gamestate === "started") {
+        if (currentSign === 0) {
+          currentSign = Math.floor(Math.random() * 26) + 1
         }
-
-        if (
-          estimatedGestures.gestures !== undefined &&
-          estimatedGestures.gestures.length > 0
-        ) {
-          const confidence = estimatedGestures.gestures.map(p => p.confidence)
-          const maxConfidence = confidence.indexOf(
-            Math.max.apply(undefined, confidence)
-          )
-
-          //setting up game state, looking for thumb emoji
-          if (
-            estimatedGestures.gestures[maxConfidence].name === "thumbs_up" &&
-            gamestate !== "played"
-          ) {
-            _signList()
-            gamestate = "played"
-            document.getElementById("emojimage").classList.add("play")
-            document.querySelector(".tutor-text").innerText =
-              "make a hand gesture based on letter shown below"
-          } else if (gamestate === "played") {
-            document.querySelector("#app-title").innerText = ""
-
-            //looping the sign list
-            if (currentSign === signList.length) {
-              _signList()
-              currentSign = 0
-              return
-            }
-
-            // console.log(signList[currentSign].src.src)
-
-            //game play state
-
-            if (
-              typeof signList[currentSign].src.src === "string" ||
-              signList[currentSign].src.src instanceof String
-            ) {
-              document
-                .getElementById("emojimage")
-                .setAttribute("src", signList[currentSign].src.src)
-              if (
-                signList[currentSign].alt ===
-                estimatedGestures.gestures[maxConfidence].name
-              ) {
-                currentSign++
-              }
-              setSign(estimatedGestures.gestures[maxConfidence].name)
-            }
-          } else if (gamestate === "finished") {
-            return
-          }
+        if (gesture.gestures[maxConfidence].name === Handsigns[currentSign].name) {
+          setSign("pass")
+          currentSign = 0
         }
       }
-      // Draw hand lines
-      const ctx = canvasRef.current.getContext("2d")
-      drawHand(hand, ctx)
     }
+
+    // Draw mesh
+    const ctx = canvasRef.current.getContext("2d")
+    drawHand(hand, ctx)
   }
+}
+}
 
-  //   if (sign) {
-  //     console.log(sign, Signimage[sign])
-  //   }
+const toggleCam = () => {
+setCamState(camState === "on" ? "off" : "on")
+}
 
-  useEffect(() => {
-    runHandpose()
-  }, [])
-
-  function turnOffCamera() {
-    if (camState === "on") {
-      setCamState("off")
-    } else {
-      setCamState("on")
-    }
-  }
-
-  return (
-    <ChakraProvider>
-      <Metatags />
-      <Box bgColor="#5784BA">
-        <Container centerContent maxW="xl" height="100vh" pt="0" pb="0">
-          <VStack spacing={4} align="center">
-            <Box h="20px"></Box>
-            <Heading
-              as="h3"
-              size="md"
-              className="tutor-text"
-              color="white"
-              textAlign="center"
-            ></Heading>
-            <Box h="20px"></Box>
-          </VStack>
-
-          <Heading
-            as="h1"
-            size="lg"
-            id="app-title"
-            color="white"
-            textAlign="center"
-          >
-            🧙‍♀️ Loading the Magic 🧙‍♂️
-          </Heading>
-
-          <Box id="webcam-container">
-            {camState === "on" ? (
-              <Webcam id="webcam" ref={webcamRef} />
-            ) : (
-              <div id="webcam" background="black"></div>
-            )}
-
-            {sign ? (
-              <div
-                style={{
-                  position: "absolute",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  right: "calc(50% - 50px)",
-                  bottom: 100,
-                  textAlign: "-webkit-center",
-                }}
-              >
-                <Text color="white" fontSize="sm" mb={1}>
-                  detected gestures
-                </Text>
-                <img
-                  alt="signImage"
-                  src={
-                    Signimage[sign]?.src
-                      ? Signimage[sign].src
-                      : "/loveyou_emoji.svg"
-                  }
-                  style={{
-                    height: 30,
-                  }}
-                />
-              </div>
-            ) : (
-              " "
-            )}
-          </Box>
-
-          <canvas id="gesture-canvas" ref={canvasRef} style={{}} />
-
-          <Box
-            id="singmoji"
-            style={{
-              zIndex: 9,
-              position: "fixed",
-              top: "50px",
-              right: "30px",
-            }}
-          ></Box>
-
-          <Image h="150px" objectFit="cover" id="emojimage" />
-          {/* <pre className="pose-data" color="white" style={{position: 'fixed', top: '150px', left: '10px'}} >Pose data</pre> */}
-        </Container>
-
-        <Stack id="start-button" spacing={4} direction="row" align="center">
-          <Button
-            leftIcon={
-              camState === "on" ? (
-                <RiCameraFill size={20} />
-              ) : (
-                <RiCameraOffFill size={20} />
-              )
-            }
-            onClick={turnOffCamera}
-            colorScheme="orange"
-          >
-            Camera
-          </Button>
-          <About />
-        </Stack>
-      </Box>
-    </ChakraProvider>
-  )
+return (
+<ChakraProvider>
+<Metatags />
+<Container maxW="container.lg">
+<Stack spacing={8} align="center">
+<Box bg="gray.100" w="100%" h="lg" position="relative">
+<Webcam
+ref={webcamRef}
+style={{
+position: "absolute",
+top: 0,
+left: 0,
+right: 0,
+bottom: 0,
+}}
+/>
+<canvas
+ref={canvasRef}
+style={{
+position: "absolute",
+top: 0,
+left: 0,
+right: 0,
+bottom: 0,
+}}
+/>
+<Signimage sign={sign} />
+<Signpass sign={sign} />
+<Box
+position="absolute"
+bottom={4}
+left={4}
+onClick={() => toggleCam()}
+cursor="pointer"
+>
+{camState === "on" ? <RiCameraOffFill size={32} /> : <RiCameraFill size={32} />}
+</Box>
+</Box>
+<VStack spacing={4}>
+<Heading size="lg">Can you sign it?</Heading>
+<Text>Make the sign you see on the screen!</Text>
+<Button onClick={() => (gamestate = "started")} colorScheme="green">
+Start
+</Button>
+<Button onClick={() => (gamestate = "stopped")} colorScheme="red">
+Stop
+</Button>
+</VStack>
+<About />
+</Stack>
+</Container>
+</ChakraProvider>
+)
 }
